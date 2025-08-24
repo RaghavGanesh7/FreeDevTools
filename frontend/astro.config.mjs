b5956d26304485c87676c762c8094eb2fafcecd0
lovestaco <athreyac4@gmail.com>
2025-08-24 15:46:25 +0530

    feat: add shadcn

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/freedevtools/t",
+  base: '/freedevtools/t',
   integrations: [react(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
