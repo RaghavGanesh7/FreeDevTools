064741c26d894f39e9af86adc1f4826e8c2aa303
lovestaco <athreyac4@gmail.com>
2025-08-24 15:46:14 +0530

    fix: tool name

--- frontend/src/components/tool/ToolHead.tsx
+++ frontend/src/components/tool/ToolHead.tsx
@@ -14,7 +14,7 @@ const ToolHead: React.FC<ToolHeadProps> = ({ name, description }) => {
         <AdBanner />
       </div>
       <div className="mb-6">
-        <p className="text-xl md:text-2xl lg:text-3xl font-medium mb-2 text-black dark:text-slate-300">
+        <p className="text-xl md:text-2xl lg:text-2xl font-medium mb-2 text-black dark:text-slate-300">
           {name}
         </p>
         <p className="text-muted-foreground ">
