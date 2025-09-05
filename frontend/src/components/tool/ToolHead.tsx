90c452c899971f764fed62dc2d283e86e0d9a471
Rijul TP <rijulrajtkeey2@gmail.com>
2025-09-05 21:50:01 +0530

    Fixing spacing and tool name size

--- frontend/src/components/tool/ToolHead.tsx
+++ frontend/src/components/tool/ToolHead.tsx
@@ -10,13 +10,13 @@ const ToolHead: React.FC<ToolHeadProps> = ({ name, description }) => {
   return (
     <>
       {/* Ad Banner */}
-      <div className="mb-24 mt-20 ">
+      <div className="mb-16 mt-8 ">
         <AdBanner />
       </div>
       <div className="mb-2">
-        <h1 className="text-xl md:text-2xl lg:text-2xl font-medium mb-2 text-black dark:text-slate-300">
+        <h2 className="text-xl md:text-2xl lg:text-2xl font-medium mb-2 text-black dark:text-slate-300">
           {name}
-        </h1>
+        </h2>
         <h3 className="text-muted-foreground ">{description}</h3>
       </div>
     </>
