a408c30fdb56b3e46963ffd4a17fded425ec79eb
lovestaco <athreyac4@gmail.com>
2025-09-06 21:25:33 +0530

    fix: spacing

--- frontend/src/components/tool/ToolHead.tsx
+++ frontend/src/components/tool/ToolHead.tsx
@@ -10,14 +10,16 @@ const ToolHead: React.FC<ToolHeadProps> = ({ name, description }) => {
   return (
     <>
       {/* Ad Banner */}
-      <div className="mb-16 mt-8 ">
+      <div className="mb-16 mt-6 ">
         <AdBanner />
       </div>
       <div className="mb-2">
         <h1 className="text-2xl font-medium mb-2 text-black dark:text-slate-300">
           {name}
         </h1>
-        <p className="text-muted-foreground text-base md:text-lg lg:text-xl font-normal ">{description}</p>
+        <p className="text-muted-foreground text-base md:text-lg lg:text-xl font-normal ">
+          {description}
+        </p>
       </div>
     </>
   );
