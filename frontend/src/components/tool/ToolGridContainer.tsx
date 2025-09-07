401830f4334aebc14b18bb200ea74d4d53dbf363
lovestaco <athreyac4@gmail.com>
2025-09-07 15:41:37 +0530

    fix: revert toolgrid issue

--- frontend/src/components/tool/ToolGridContainer.tsx
+++ frontend/src/components/tool/ToolGridContainer.tsx
@@ -2,32 +2,16 @@ const ToolGridContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
       className="
-        flex
-        flex-col
-        gap-6
-        max-w-[1600px]
-        mx-auto
-        mt-8
-        xl:flex-row
-        xl:items-start
-      "
+          grid
+          grid-cols-1
+          xl:grid-cols-2
+          gap-6
+          max-w-[1600px]
+          mx-auto
+          mt-8
+        "
     >
-      {/* Single column on mobile/tablet */}
-      <div className="flex flex-col gap-6 xl:hidden">{children}</div>
-
-      {/* Two columns on large screens */}
-      <div className="hidden xl:flex xl:flex-col xl:gap-6 xl:flex-1">
-        {/* Left column items */}
-        {Array.isArray(children)
-          ? children.filter((_, index) => index % 2 === 0)
-          : [children[0]]}
-      </div>
-      <div className="hidden xl:flex xl:flex-col xl:gap-6 xl:flex-1">
-        {/* Right column items */}
-        {Array.isArray(children)
-          ? children.filter((_, index) => index % 2 === 1)
-          : [children[1]]}
-      </div>
+      {children}
     </div>
   );
 };
