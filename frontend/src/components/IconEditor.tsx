1ed1fdb1580764fa484edf17ccac146b6d0166a1
lovestaco <athreyac4@gmail.com>
2025-09-18 20:11:45 +0530

    fix rm konva editor

--- frontend/src/components/IconEditor.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { IconEditorProps } from './icon-editor';
-import KonvaCanvas from './icon-editor/KonvaCanvas';
-
-const IconEditor: React.FC<IconEditorProps> = (props) => {
-  return <KonvaCanvas {...props} />;
-};
-
-export default IconEditor;
