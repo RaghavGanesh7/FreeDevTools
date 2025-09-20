cd55035f7c2871f68c8e88b1b69098254e89464a
lovestaco <athreyac4@gmail.com>
2025-09-20 21:00:21 +0530

    fix: use img tag for crawling

--- frontend/src/pages/svg_icons/sitemap.xml.ts
+++ frontend/src/pages/svg_icons/sitemap.xml.ts
@@ -9,7 +9,7 @@ export const GET: APIRoute = async ({ site }) => {
   // Get all SVG files
   const svgFiles = await glob("**/*.svg", { cwd: "./public/svg_icons" });
 
-  // Map files to sitemap URLs
+  // Map files to sitemap URLs with image information
   const urls = svgFiles.map((file) => {
     const parts = file.split(path.sep);
     const name = parts.pop()!.replace(".svg", "");
@@ -20,6 +20,11 @@ export const GET: APIRoute = async ({ site }) => {
         <lastmod>${now}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.4</priority>
+        <image:image xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+          <image:loc>${site}/svg_icons/${category}/${name}.svg</image:loc>
+          <image:title>Free ${name} SVG Icon Download</image:title>
+          <image:caption>Download ${name} SVG icon for free. PNG icon. No registration required. Unlimited Downloads.</image:caption>
+        </image:image>
       </url>`;
   });
 
@@ -34,7 +39,7 @@ export const GET: APIRoute = async ({ site }) => {
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="/freedevtools/sitemap.xsl"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   ${urls.join("\n")}
 </urlset>`;
 
